08c41464ea4c70a736304251de484ba89d8377a1
yqsqq <yqsqq@163.com>
2013-07-03 18:40:52 +0800

    [YMZG-5][Qingsong] move repo to ymzg organization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ymzg::Application.config.secret_key_base = 'e14573e5a61bd6d5a3a8b72bf544714e215502856c2da5fc91a188c1039dd212e99f9068d1c2e1453768a94ebac469c91401279a4318663aa7d5268916fa449c'
