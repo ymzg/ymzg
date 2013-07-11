b6860aca480d99ecd83c3dec50dc402183f0bc00
Si Xiaojing <sixiaojing1988@gmail.com>
2013-07-11 18:15:35 +0800

    add test for events controller.

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,4 +2,26 @@ require 'spec_helper'
 
 describe EventsController do
 
+  describe "GET #index" do
+    it "responds successfully with an http 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response.status).to eq(200)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "loads all of the events into @events" do
+      event1, event2 = Event.create!, Event.create!
+      get :index
+
+      expect(assigns(:events)).to match_array([event1, event2])
+    end
+  end
+
+
+
 end
