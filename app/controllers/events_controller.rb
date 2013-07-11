af5922e5557dd403c16c8d7b42079c84f406a9e5
Si Xiaojing <sixiaojing1988@gmail.com>
2013-07-11 16:38:45 +0800

    add events controller. now can publish a event and view it.

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,20 @@
+class EventsController < ApplicationController
+
+  def create
+    #render text: params[:event].inspect
+    @event = Event.new(event_params)
+    @event.save
+
+    redirect_to @event
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :describe, :data, :location)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Ymzg::Application.routes.draw do
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :events
+
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
