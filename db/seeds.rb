f2b8d50f8850b356d56b9719efd6c073e229dc1f
junv <wahyd4@gmail.com>
2013-07-20 15:50:54 +0800

    improve UX of login

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,4 @@
 
 Event.create(:title => "情系汶川", :description => "去汶川地区义教", :event_date => Time.now)
 Event.create(:title => "情系山区", :description => "去雅安山区义教", :event_date => Time.now)
+User.create username: 'root', password: 'root', is_admin: true
