33c4e1fcc70c79079fa05950be200debc4313904
Haiyang He <hyhe@thoughtworks.com>
2013-07-20 15:29:35 +0800

    [yanjiao & haiyang]修改Event时间

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Event.create(:title => "情系汶川", :description => "去汶川地区义教", :event_date => Time.now)
+Event.create(:title => "情系汶川", :description => "去汶川地区义教", :event_date => Time.gm(2013,7,10,22,5,0))
 Event.create(:title => "情系山区", :description => "去雅安山区义教", :event_date => Time.now)
