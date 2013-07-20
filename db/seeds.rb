3a9ad7fead9d723476d9e4d66787434e3c7ed9f9
Haiyang He <hyhe@thoughtworks.com>
2013-07-20 13:27:34 +0800

    [yanjiao & haiyang]添加Event数据到seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Event.create(:title => "Test", :description => "Test Description", :event_date => Time.now)
