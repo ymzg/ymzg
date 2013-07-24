76ac42db0d2a50f10e706d10420af0fb67b17480
Si Xiaojing <sixiaojing1988@gmail.com>
2013-07-24 20:29:25 +0800

    Xiaojing: show original image on event detail

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,8 @@ class Event < ActiveRecord::Base
   scope :all_events, -> {
     find(:all, :order => "event_date desc")
   }
-  attr_accessible :image, :location, :event_date, :title
+  attr_accessible :image, :location, :event_date, :title, :description
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/img/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /image/
   has_many :comments # , foreign_key: "event_id"
 end
