56031fa048f55daab331769b8960d2e74f2e70bb
nanchu <nan.chu@fourthkingdom.net>
2013-07-30 23:03:21 +0800

    adding images to story

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -9,9 +9,12 @@ def story_1_header
 			content_tag(:p, "我一直在想我应该写些什么来记录这次的活动，又应该怎么写才能把自己表达出自己的心情，让大家都感觉得到这次活动的意义，明白‘义梦中国’这个团队的努力奋斗的目标。")
 		end
 end
+def story_1_hero
+	content_tag(:div, "", :class=>"hero-image", :style=>"background-image:url('/img/yingxiu-trip-4.jpeg');")
+end
+
 def story_1_body
 	content_tag(:section, :class=>'story-body') do
-
 		content_tag(:h2, "到达映秀")+
 		content_tag(:p, "现在距离5·12大地震已经过去整整五年多了，四面被急流环绕的映秀镇也是日益漂亮。刚到映秀镇的那天下午，我就被它的美丽所吸引，这里所有的建筑都是以藏族风格，羌族风格，川西居民风格三大风格修建的，象征真映秀人民都是一家人，大家共同努力，把家园建设的更加美丽。晚饭后，在梁老师的安排下，我和映菊住进了袁芳家，给了我彻彻底底了解映秀的机会，这晚我感觉自己也成了映秀的一份子。第二天，我们早早的起了床，吃了大厨袁芳烧的早饭，我们一行人在梁老师的带领下，乘车来到了半山腰的居民家。")+
 
@@ -28,6 +31,17 @@ def story_1_body
 		content_tag(:p, "第三天，我们的活动也正式开始了，大家都以积极的心期待着活动的顺利进行，不过天有不测风云，刚开始出了点失误，导致气愤有点紧张，不过很快雨过天晴，所有的人齐聚一堂，听梁老师做出的关于人生职业规划，义梦中国这个团队的形成初衷和发展方向的演讲；听李老师做出的关于如何快乐教学，让孩子们都能开心的学到知识得演讲；听熊哥做出的（ThoughtWorks公司的总监，也是此次活动赞助商的代表人）的现实剖析，应该如何做才能让教育事业蒸蒸日上，为社会做贡献的演讲；以及厅青少年活动中心负责人李老师做出的的汶川经历，如何从中恢复过来，发展自己的事业的演讲等等。演讲过程十分的精彩，老师们或多或少受益其中，我也是从中懂得了很多，人生不可能一翻风顺，只因困难变得更加美丽，如果你一味的认命，不作出自己的努力，那么你注定一事无成。现实的残酷只会把我们变得越来越有战斗力，映秀的人民做到了，那么庐山的人民也就更不在害怕努力白费，希望老师们都能快快的好起来，孩子们都还等着你们。庐山加油！雅安加油！四川加油！此次活动还在继续，可我们几个义工因为考试的原因不得不离开，心里有点遗憾，不过我们的心仍在一起，希望这次活动能顺顺利利的继续下去。")
 
 	end	
+end
+def story_1_gallery
+	content_tag(:div, :class=>"story-gallery")do
+		image_tag("/img/yingxiu-trip-1.jpeg")+
+		image_tag("/img/yingxiu-trip-2.jpeg")+
+		image_tag("/img/yingxiu-trip-3.jpeg")+
+		image_tag("/img/yingxiu-trip-4.jpeg")+
+		image_tag("/img/yingxiu-trip5.jpeg")+
+		image_tag("/img/yingxiu-zhenghou.jpg")+
+		image_tag("/img/yingxiu.jpg")
+	end
 end
 
 def story_2_header
