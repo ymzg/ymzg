6dd8952caffeba0e9b447d7b4a87a1f74ae032c5
sea <sea@sea-ThinkPad-E425.(none)>
2013-08-02 18:58:20 +0800

    improve the footer

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20130720053139) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "events", force: true do |t|
     t.string   "title"
     t.string   "location"
