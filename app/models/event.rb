cd37c781848dd2a1861ad308502c968335ab3484
nanchu <nan.chu@fourthkingdom.net>
2013-08-15 01:24:05 +0800

    updating styles for events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,9 @@
 #encoding: utf-8
 class Event < ActiveRecord::Base
   attr_accessible :image, :location, :event_date, :title, :description
-  validates :image, :location, :event_date, :title, :description, presence: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/img/:style/missing.png"
-  validates_attachment :image, :presence => true,
-                       :content_type => { :content_type => "image/png" },
-                       :size => { :in => 0..3000.kilobytes }
+  validates  :location, :event_date, :title, :description, presence: true
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "300x300>" }, :default_url => "/img/:style/missing.png"
+  
 
   scope :all_events, -> {
     find(:all, :order => "event_date desc")
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130720053139) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: true do |t|
     t.string   "title"
     t.string   "location"
