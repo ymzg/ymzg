571252c97fed74080ddc8c341bc92a36302cb005
nanchu <nan.chu@fourthkingdom.net>
2013-08-15 02:28:01 +0800

    adding a lot of styling for creating events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :image, :location, :event_date, :title, :description
   validates  :location, :event_date, :title, :description, presence: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "300x300>" }, :default_url => "/img/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "200x200>" }, :default_url => "/img/:style/missing.png"
   
 
   scope :all_events, -> {
