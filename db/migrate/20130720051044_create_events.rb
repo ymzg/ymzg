4159e57a75f3b325c1adbe1bce54ca3d51ec3728
yqsqq <yqsqq@163.com>
2013-11-07 14:57:32 +0800

    modify the migration file

--- db/migrate/20130720051044_create_events.rb
+++ db/migrate/20130720051044_create_events.rb
@@ -1,13 +1,18 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-      t.string :title
-      t.string :location
-      t.text :description
-      t.attachment :image
-      t.date :event_date
-      
-      t.timestamps
+
+      t.string   "title"
+      t.string   "location"
+      t.text     "description"
+      t.string   "image_file_name"
+      t.string   "image_content_type"
+      t.integer  "image_file_size"
+      t.datetime "image_updated_at"
+      t.date     "event_date"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.string   "event_url
     end
   end
 end
