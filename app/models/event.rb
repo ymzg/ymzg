c0658e566c202fab643135cff17e2c2491d5d197
yqsqq <yqsqq@163.com>
2013-11-21 20:20:44 +0800

    [qingsong] fix the missing default picture of event

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,7 +2,7 @@
 class Event < ActiveRecord::Base
   attr_accessible :image, :location, :event_date, :title, :description, :event_url
   validates  :location, :event_date, :title, :description, :event_url, presence: true
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "200x200>" }, :default_url => "/img/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "200x200>" }, :default_url => "/img/thumb/missing.png"
   
 
   scope :all_events, -> {
